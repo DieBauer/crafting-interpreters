72e22538fbf273827b7e0c2daa63749009b48ec6
Jens <jenskat@gmail.com>
2022-07-02 13:56:12 +0200

    Add error productions

--- src/interpreter/java/kat/jens/lox/Parser.java
+++ src/interpreter/java/kat/jens/lox/Parser.java
@@ -26,21 +26,21 @@ class Parser {
 
     private Expr expression() {
         return conditional();
-      }
-      
-      private Expr conditional() {
+    }
+
+    private Expr conditional() {
         Expr expr = equality();
-      
+
         if (match(QUESTION)) {
-          Expr thenBranch = expression();
-          consume(COLON,
-              "Expect ':' after then branch of conditional expression.");
-          Expr elseBranch = conditional();
-          expr = new Expr.Conditional(expr, thenBranch, elseBranch);
+            Expr thenBranch = expression();
+            consume(COLON,
+                    "Expect ':' after then branch of conditional expression.");
+            Expr elseBranch = conditional();
+            expr = new Expr.Conditional(expr, thenBranch, elseBranch);
         }
-      
+
         return expr;
-      }
+    }
 
     private Expr comma() {
         Expr expr = equality();
@@ -124,6 +124,31 @@ class Parser {
             Expr expr = expression();
             consume(RIGHT_PAREN, "Expect ')' after expression.");
             return new Expr.Grouping(expr);
+        }
+
+        // Error productions.
+        if (match(BANG_EQUAL, EQUAL_EQUAL)) {
+            error(previous(), "Missing left-hand operand.");
+            equality();
+            return null;
+        }
+
+        if (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
+            error(previous(), "Missing left-hand operand.");
+            comparison();
+            return null;
+        }
+
+        if (match(PLUS)) {
+            error(previous(), "Missing left-hand operand.");
+            term();
+            return null;
+        }
+
+        if (match(SLASH, STAR)) {
+            error(previous(), "Missing left-hand operand.");
+            factor();
+            return null;
         }
 
         throw error(peek(), "Expect expression.");
